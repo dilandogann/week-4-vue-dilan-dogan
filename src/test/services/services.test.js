290d3a676f2cde9e65c08ecd6f2fb054fe659b8b
dilan doğan <dilan.dogan@turkai.com>
2021-08-04 15:00:53 +0300

    test case bug fix

--- src/test/services/services.test.js
+++ src/test/services/services.test.js
@@ -8,21 +8,23 @@ describe("Service tests", () => {
   it("should return all starships", async () => {
     axios.get.mockImplementation(() => {
       return Promise.resolve({
-        count: 36,
-        next: "https://swapi.dev/api/starships/?page=2",
-        previous: null,
-        results: [
-          {
-            model: "CR90 corvette",
-            name: "CR90 corvette",
-            url: "https://swapi.dev/api/starships/1/"
-          },
-          {
-            model: "Sentinel-class landing craft",
-            name: "Sentinel-class landing craft",
-            url: "https://swapi.dev/api/starships/2/"
-          }
-        ]
+        data: {
+          count: 36,
+          next: "https://swapi.dev/api/starships/?page=1",
+          previous: null,
+          results: [
+            {
+              model: "CR90 corvette",
+              name: "CR90 corvette",
+              url: "https://swapi.dev/api/starships/1/"
+            },
+            {
+              model: "Sentinel-class landing craft",
+              name: "Sentinel-class landing craft",
+              url: "https://swapi.dev/api/starships/2/"
+            }
+          ]
+        }
       }
       );
     });
