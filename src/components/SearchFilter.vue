345127c3d8188a544f406251556ff194b9989bc1
dilan doğan <dilan.dogan@turkai.com>
2021-08-09 18:09:36 +0300

    Filter button changes

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -4,15 +4,18 @@
       <v-text-field
         v-model="searchText"
         class="custom-label-color"
-        label="Search"
+        label="Search For..."
         outlined
         clearable
       ></v-text-field>
+
       <v-btn
-        class="filterButton"
+        class="filterButton button-margin"
         color="#9c27b0"
         @click="$emit('filterResults', searchText)"
-        ><span style="color: wheat"> Search</span>
+      >
+        <v-icon class="wheat-color">mdi-magnify</v-icon>
+        <span class="button-text wheat-color"> Filter</span>
       </v-btn>
     </v-row>
   </div>
@@ -27,29 +30,30 @@ export default {
   },
 };
 </script>
-<style scoped> 
+<style scoped>
 .v-text-field--outlined >>> fieldset {
   border-color: rgba(192, 0, 250, 0.986);
 }
-.theme--light.v-input{
-  color:rgba(192, 0, 250, 0.986) !important;
+.theme--light.v-input {
+  color: rgba(192, 0, 250, 0.986) !important;
 }
-.custom-placeholer-color input::placeholder {
-  color: red!important;
-  opacity: 1;
-}
-
-.custom-label-color .v-label {
-  color: red;
-  opacity: 1;
-}
-
-.custom-placeholer-color input,
-.custom-label-color input{
-  color: red!important;
+/deep/.theme--light.v-label {
+  color: aliceblue !important;
+  font-size: 16px;
 }
 .filterButton {
   margin-left: 12px;
   height: 54px;
+}
+.v-btn>.v-btn__content .v-icon {
+    color: aliceblue !important;
+}.button-text{
+  font-weight: 600;
+}
+.wheat-color{
+  color: wheat
+}
+.button-margin{
+  margin-top: 10px;
 }
 </style>
